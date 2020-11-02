77a8e5e570b5e179cf5c7992c446cc502ea965ae
Your Name <you@example.com>
2020-11-02 11:32:50 +0800

    first commit

---
+++ spr-maven-mvc-tiles-app-10-23/src/main/java/com/spring/MyController.java
@@ -0,0 +1,35 @@
+package com.spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MyController {
+
+	@RequestMapping(value = { "/login"}, method = RequestMethod.POST)
+	public String loginPage(ModelMap model) {
+		return "login";
+	}
+	
+	@RequestMapping(value = { "/home"}, method = RequestMethod.GET)
+	public String homePage(ModelMap model) {
+		return "home";
+	}
+
+	@RequestMapping(value = { "/products"}, method = RequestMethod.GET)
+	public String productsPage(ModelMap model) {
+		return "products";
+	}
+
+	@RequestMapping(value = { "/contactus"}, method = RequestMethod.GET)
+	public String contactUsPage(ModelMap model) {
+		return "contactus";
+	}
+	
+	@RequestMapping(value = { "/technologies"}, method = RequestMethod.GET)
+	public String technologiesPage(ModelMap model) {
+		return "technologies";
+	}
+}
---
+++ spr-maven-mvc-tiles-app-10-23/src/main/webapp/WEB-INF/views/pages/technologies.jsp
@@ -0,0 +1,1 @@
+<h2>Technologies Page</h2>
